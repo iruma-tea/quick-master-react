79d838c20455fad861beaa6330c8ce43b0750aca
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-08-11 05:52:39 +0900

    3.1の学習完了

--- part3/jsx-basic/src/index.js
+++ part3/jsx-basic/src/index.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+// import logo from './logo.svg';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <p>Reactをはじめよう！</p>
+    <p>JSXの基本</p>
+  </div>
 );
+// root.render(
+//   <React.Fragment>
+//     <p>Reactをはじめよう！</p>
+//     <p>JSXの基本</p>
+//   </React.Fragment>
+// );
+// root.render(
+//   <>
+//     <p>Reactをはじめよう！</p>
+//     <p>JSXの基本</p>
+//   </>
+// );
+
+// const tag = <img src={logo} />
+
+// const tag = (
+//   <label htmlFor="name" className="sky">
+//     <input id="name" type="text" tabIndex="0" />
+//   </label>
+// );
+
+// const tag = (
+//   <div>
+//     {/* コメントです。 */}
+//     <p
+//     // コメントです。
+//     /*コメントです。
+//     コメントです*/
+//     >
+//       こんにちは、世界！
+//     </p>
+//   </div>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
