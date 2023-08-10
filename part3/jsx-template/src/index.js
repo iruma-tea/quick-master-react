1d95944c85b180ab7979235133cc82b7ce2c246d
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-08-11 06:21:19 +0900

    3.2の学習完了

--- part3/jsx-template/src/index.js
+++ part3/jsx-template/src/index.js
@@ -1,15 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// const name = '山田';
+// root.render(
+//   <p>こんにちは、{name}さん！</p>
+// );
+
+// root.render(
+//   <div>
+//     <p>名前は、{'$' + name + '$'}です。</p>
+//     <p>現在の日時は、{new Date().toLocaleDateString()}です。</p>
+//     <p>2の3乗は、{2 ** 3}です。</p>
+//   </div>
+// );
+
+// const str = 'こんにちは、世界！<br />さようなら、私';
+// root.render(
+//   // <p>{str}</p>
+//   <p dangerouslySetInnerHTML={{__html: str}}></p>
+// );
+
+// const url = 'https://wings.msn.to/';
+// root.render(
+//   <a href={url}>サポートサイト</a>
+// );
+
+// const style = {color: 'Red', backgroundColor: 'Yellow' };
+// root.render(
+//   <p style={style}>WINGSプロジェクト</p>
+// );
+
+const attrs = {
+  src: 'https://wings.msn.to/image/wings.jpg',
+  alt: '',
+  title: 'WINGSプロジェクト'
+};
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <img {...attrs} />
 );
+
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
