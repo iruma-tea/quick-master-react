7256d9728bc4f6f586a1e2040ea39d2debc2879b
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-08-11 06:31:12 +0900

    part3の学習終了

--- part3/jsx-create/src/index.js
+++ part3/jsx-create/src/index.js
@@ -5,10 +5,33 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// const name = 'WINGSプロジェクト'; 
+// const tag = (
+//   <div className="main">
+//     <h1>ようこそ、{name}へ！</h1>
+//     <img src="http://www.wings.msn.to/image/wings.jpg" />
+//     こんにちは、世界！
+//   </div>
+// );
+
+const name = 'WINGSプロジェクト'; 
+const tag = React.createElement(
+  'div',
+  { className: 'main' },
+  React.createElement(
+    'h1',
+    null,
+    `ようこそ、${name}へ`
+  ),
+  React.createElement(
+    'img',
+    { src: 'http://www.wings.msn.to/image/wings.jpg' }
+  ),
+  'こんにちは、世界！'
+);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>{tag}</div>
 );
 
 // If you want to start measuring performance in your app, pass a function
