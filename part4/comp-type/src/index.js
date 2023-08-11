0dbdf8bd1a238831bcc551426d1b8ab6dfe53c62
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-08-11 18:42:55 +0900

    4.2.1 さまざまな型をコンポーネントに渡す
    サンプルの作成完了

--- part4/comp-type/src/index.js
+++ part4/comp-type/src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MyType from './MyType';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <MyType value="山田" />
+    <MyType value={'鈴木'} />
+    <MyType value={ 108 } />
+    <MyType value={ true } />
+    <MyType value={ ['うめ', 'もも', 'さくら'] } />
+    <MyType value={ {name: '山田太郎', age: 40} } />
+    <MyType value={ () => console.log('Hoge') } />
+  </div>
 );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
