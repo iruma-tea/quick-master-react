f14256e9138052388672619ecdff4d01e11972b6
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-08-11 19:26:20 +0900

    4.2.4 コンポーネントを入れ子にする
    サンプルの作成完了

---
+++ part4/comp-nest/src/MyCover.js
@@ -0,0 +1,4 @@
+export default function MyCover(props) {
+  const url = `https://wings.msn.to/books/${props.isbn}/${props.isbn}.jpg`;
+  return <img src={url} alt={props.title} />;
+}
--- part4/comp-nest/src/index.js
+++ part4/comp-nest/src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from './App';
+import reportWebVitals from "./reportWebVitals";
+import MyBook from "./MyBook";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const book = {
+  isbn: "WGS-JST-002",
+  title: "速習webpack 第2版",
+  price: 500,
+  published: "WINGSプロジェクト",
+};
+root.render(<MyBook info={book} />);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
