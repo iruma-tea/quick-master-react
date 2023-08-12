89885ba011e7468e5e605d0957177d2995031f8b
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-08-12 20:56:01 +0900

    7.5 チェックボックス(単一)
    サンプルの作成完了

---
+++ part7/form-check/src/MyCheck.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default function MyCheck() {
+  const [form, setForm] = useState({
+    send: true,
+  });
+
+  function handleChangeCheck(e) {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.checked,
+    });
+  }
+
+  function show() {
+    console.log(`メール送付: ${form.send ? "有効" : "無効"}`);
+  }
+
+  return (
+    <form>
+      <label htmlFor="send">メール送付?:</label>
+      <input
+        type="checkbox"
+        name="send"
+        id="send"
+        checked={form.send}
+        onChange={handleChangeCheck}
+      />
+      <br />
+      <button type="button" onClick={show}>
+        送信
+      </button>
+    </form>
+  );
+}
