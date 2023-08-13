5784195533e3a4cb7d1dfba2bf1ba1ed844021c5
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-08-13 18:51:47 +0900

    8.1ルータの基本
    サンプルの作成完了

--- README.md
+++ README.md
@@ -7,3 +7,6 @@
 ## npm run build : プロジェクトのビルド
 ## npm run test : テストを実行
 ## npm run eject : 設定情報を出力
+
+## ルーティング ライブラリ React Router
+[コマンド]: npm install --save react-router-dom
--- part8/route-basic/src/index.js
+++ part8/route-basic/src/index.js
@@ -1,15 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import MyTop from "./MyTop";
+import MyHello from "./MyHello";
+import MyArticle from "./MyArticle";
+import NotFound from "./NotFound";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="" element={<MyTop />} />
+      <Route path="hello" element={<MyHello />} />
+      <Route path="article" element={<MyArticle />} />
+      <Route path="*" element={<NotFound />} />
+    </>
+  )
 );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
