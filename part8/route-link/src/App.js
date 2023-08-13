bbebb1dc0a97d7e7eac86c54eb5593249a917fbb
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-08-13 19:44:55 +0900

    8.2 ルータ対応のリンクの設定
    サンプルの作成完了

--- part8/route-link/src/App.js
+++ part8/route-link/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Outlet } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        <li>
+          <Link to="/">トップ</Link>
+        </li>
+        <li>
+          <Link to="/hello">Hello</Link>
+        </li>
+        <li>
+          <Link to="/article">公開記事</Link>
+        </li>
+      </ul>
+      <hr />
+      <Outlet />
+    </>
   );
 }
 
--- part8/route-link/src/index.js
+++ part8/route-link/src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Mytop from "./MyTop";
+import MyHello from "./MyHello";
+import MyArticle from "./MyArticle";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Mytop />} />
+      <Route path="/hello" element={<MyHello />} />
+      <Route path="/article" element={<MyArticle />} />
+    </Route>
+  )
 );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
