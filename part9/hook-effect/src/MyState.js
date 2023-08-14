34db5c27b3fa753ded93595d31d9efa78bab81fb
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-08-15 08:10:30 +0900

    9.1.1 副作用フックの基本
    サンプルの作成完了

---
+++ part9/hook-effect/src/MyState.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+export default function MyState(props) {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(`${count}回、クリックされました。`);
+  }, [count]);
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  return (
+    <>
+      <input type="button" value="カウント" onClick={increment} />
+      <p>{count}回、クリックされました。</p>
+    </>
+  );
+}
