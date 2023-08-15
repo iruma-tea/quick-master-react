35048879ab9eaefe3c45cf9236b9243689160420
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-08-15 19:10:16 +0900

    9.3.4 コンポーネント結果のキャッシング
    サンプルの作成完了

--- part9/hook-context-separate/src/MyCount3.js
+++ part9/hook-context-separate/src/MyCount3.js
@@ -1,19 +1,22 @@
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { MyAppContext } from "./MyAppContext";
 
 export default function MyCount3() {
   const { count, setCount } = useContext(MyAppContext);
-  return (
-    <div className="border">
-      MyCount3
-      <div>
-        <input
-          type="button"
-          value="カウント"
-          onClick={() => setCount(count + 1)}
-        />
-        <p>{count}回、クリックされました。</p>
+  return useMemo(() => {
+    console.log("MyCount3 is updated.");
+    return (
+      <div className="border">
+        MyCount3
+        <div>
+          <input
+            type="button"
+            value="カウント"
+            onClick={() => setCount(count + 1)}
+          />
+          <p>{count}回、クリックされました。</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }, [count, setCount]);
 }
---
+++ part9/hook-context-separate/src/MyCount4.js
@@ -0,0 +1,23 @@
+import { useContext, useMemo } from "react";
+import { MyAppContext } from "./MyAppContext";
+
+export default function MyCount4() {
+  const { count2, setCount2 } = useContext(MyAppContext);
+
+  return useMemo(() => {
+    console.log("MyCount4 is updated.");
+    return (
+      <div className="border">
+        MyCount4
+        <div>
+          <input
+            type="button"
+            value="カウント2"
+            onClick={() => setCount2(count2 + 1)}
+          />
+          <p>{count2}回、クリックされました。</p>
+        </div>
+      </div>
+    );
+  }, [count2, setCount2]);
+}
