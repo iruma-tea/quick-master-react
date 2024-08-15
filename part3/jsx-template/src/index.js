30ca02d7031283a23e3e7d6af47141d47112a71f
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-08-16 08:16:30 +0900

    JSXのサンプルを記載

--- part3/jsx-template/src/index.js
+++ part3/jsx-template/src/index.js
@@ -10,8 +10,32 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 //     <App />
 //   </React.StrictMode>
 // );
-const name = "山田";
-root.render(<p>こんにちは、{name}さん！</p>);
+// const name = "山田";
+// root.render(<p>こんにちは、{name}さん！</p>);
+// root.render(
+//   <div>
+//     <p>名前は、{"$" + name + "$"}です。</p>
+//     <p>現在の日時は、{new Date().toLocaleString()}です。</p>
+//     <p>2の3乗は、{2 ** 3}です。</p>
+//   </div>
+// );
+
+// const str = "こんにちは、世界!<br />さようなら、私";
+// root.render(<p dangerouslySetInnerHTML={{ __html: str }}></p>);
+
+// const url = "https://wings.msn.to/";
+// root.render(<a href={url}>サポートサイト</a>);
+
+// const style = { color: "Red", backgroundColor: "Yellow" };
+// root.render(<p style={style}>WINGSプロジェクト</p>);
+
+const attrs = {
+  src: "https://wings.msn.to/image/wings.jpg",
+  alt: "",
+  title: "WINGSプロジェクト",
+};
+
+root.render(<img {...attrs} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
