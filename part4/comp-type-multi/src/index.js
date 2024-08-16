d089f101de5e834c0025066a45c99ef2469f9307
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-08-16 12:55:56 +0900

    4.2.2. オブジェクトを個々の属性に展開する

---
+++ part4/comp-type-multi/src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from './App';
+import reportWebVitals from "./reportWebVitals";
+import MyAttrMulti from "./MyAttrMulti";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+const data = {
+  name: "山田権左衛門",
+  age: 18,
+  sex: "男",
+};
+root.render(<MyAttrMulti {...data} />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
