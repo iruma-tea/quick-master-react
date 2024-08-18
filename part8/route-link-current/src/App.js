fb82a5ca14285340daf3bab4952022fb51161ed8
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-08-18 18:53:40 +0900

    スタイルクラスを適用するの学習完了

--- part8/route-link-current/src/App.js
+++ part8/route-link-current/src/App.js
@@ -1,7 +1,9 @@
 import { NavLink, Outlet } from "react-router-dom";
+import "./App.css";
 
 function isCurrent(link) {
-  return link.isActive ? { backgroundColor: "Yellow" } : undefined;
+  // return link.isActive ? { backgroundColor: "Yellow" } : undefined;
+  return link.isActive ? "current" : undefined;
 }
 
 export default function App() {
@@ -9,17 +11,20 @@ export default function App() {
     <>
       <ul>
         <li>
-          <NavLink style={isCurrent} to="/">
+          {/* <NavLink style={isCurrent} to="/"> */}
+          <NavLink className={isCurrent} end to="/">
             トップ
           </NavLink>
         </li>
         <li>
-          <NavLink style={isCurrent} to="/hello">
+          {/* <NavLink style={isCurrent} to="/hello"> */}
+          <NavLink className={isCurrent} to="/hello">
             Hello
           </NavLink>
         </li>
         <li>
-          <NavLink style={isCurrent} to="/article">
+          {/* <NavLink style={isCurrent} to="/article"> */}
+          <NavLink className={isCurrent} to="/article">
             公開記事
           </NavLink>
         </li>
